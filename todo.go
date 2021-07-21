5fbeecb7cf8ac215051b1add15ab30161cb63411
Christina <kris.inferno@gmail.com>
2021-07-21 17:05:15 +0300

    initial commit

---
+++ todo.go
@@ -0,0 +1,26 @@
+package todo_app
+
+type TodoList struct {
+	Id          int `json:"id"`
+	Title       int `json:"title"`
+	Description int `json:"description"`
+}
+
+type UserList struct {
+	Id     int
+	UserId int
+	ListId int
+}
+
+type TodoItem struct {
+	Id          int `json:"id"`
+	Title       int `json:"title"`
+	Description int `json:"description"`
+	Done        int `json:"done"`
+}
+
+type ListsItem struct {
+	Id     int
+	ListId int
+	ItemId int
+}
