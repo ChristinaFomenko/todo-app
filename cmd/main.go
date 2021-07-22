a46148490057841d94a9b54fcda7ed9de94f0a7f
Christina <kris.inferno@gmail.com>
2021-07-22 09:15:39 +0300

    auth, handlers

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"github.com/KrisInferno/todo-app"
+	"github.com/KrisInferno/todo-app/pkg/handler"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(todo_app.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
