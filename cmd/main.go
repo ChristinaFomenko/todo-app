e86a5a3954885331064b6099001578ff9a2e048a
Christina <kris.inferno@gmail.com>
2021-07-22 11:54:43 +0300

    add

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	"github.com/KrisInferno/todo-app"
-	"github.com/KrisInferno/todo-app/pkg/handler"
 	"log"
+
+	todo_app "github.com/KrisInferno/todo-app"
+	"github.com/KrisInferno/todo-app/pkg/handler"
+	"github.com/KrisInferno/todo-app/pkg/repository"
+	"github.com/KrisInferno/todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo_app.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
