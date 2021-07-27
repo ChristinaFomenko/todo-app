17953f1b2ee952371a2b0c8dc569cf1ce5d75f41
Christina <kris.inferno@gmail.com>
2021-07-27 11:27:56 +0300

    parse jwt

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -5,6 +5,7 @@ import "github.com/KrisInferno/todo-app/pkg/repository"
 type Authorization interface {
 	CreateUser(user interface{}) (int error, err error)
 	GenerateToken(username, password string) (string, error)
+	ParseToken(token string) (int, error)
 }
 
 type TodoList interface {
